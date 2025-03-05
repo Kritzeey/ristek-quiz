92f057e6816f12edd726890b8cfc17cb0d63b8af
valhize <190005533+Kritzeey@users.noreply.github.com>
2025-03-05 11:34:32 +0700

    Test

--- app/Components/Button.js
+++ app/Components/Button.js
@@ -1,9 +1,7 @@
 
 export default function Button(props) {
     return(
-        <button className="h-[4rem] duration-250 cursor-pointer hover:text-[#09090b] hover:bg-[#fafafa] text-center text-xl flex items-center justify-center w-[14rem] border-2 border-[#fafafa]"
-            onClick={props.onClick}
-        >
+        <button className="h-[4rem] duration-250 cursor-pointer hover:text-[#09090b] hover:bg-[#fafafa] text-center text-xl flex items-center justify-center w-[14rem] border-2 border-[#fafafa]">
             {props.text}
         </button>
     );
--- app/page.js
+++ app/page.js
@@ -1,16 +1,30 @@
-"use client"
 
-import { useState } from 'react';
+
 import CardContainer from "./Components/CardContainer";
 import Menu from "./Components/Menu.js";
+import { neon } from '@neondatabase/serverless';
 
 export default function Home() {
-  const [status, setStatus] = useState("menu");
+  async function create(formData) {
+    "use server";
+
+    const sql = neon(`${process.env.DATABASE_URL}`);
+    const comment = formData.get('comment');
+
+    await sql('INSERT INTO comments (comment) VALUES ($1)', [comment]);
+  }
+
+  const status = "menu";
 
   return (
     <>
-      {status === "menu" && <Menu setStatus={setStatus}/>}
+      {status === "menu" && <Menu />}
       {status === "quiz" && <CardContainer />}  
+
+      <form action={create} className="flex h-screen bg-[#fafafa]">
+        <input type="text" placeholder="write a comment" name="comment" />
+        <button type="submit">Submit</button>
+      </form>
     </>
   );
 }
