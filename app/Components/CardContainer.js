d0599deec7073d3c90ee5427f942b8f55b2a0981
valhize <190005533+Kritzeey@users.noreply.github.com>
2025-03-06 16:23:02 +0700

    Polished CR operations; data is now rendered as quiz cards

--- app/Components/CardContainer.js
+++ app/Components/CardContainer.js
@@ -1,28 +1,24 @@
 import Card from "./Card";
+import { neon } from "@neondatabase/serverless";
 
-export default function CardContainer() {
-    const quizList = [
-        {
-            id: "1",
-            title: "JavaScript",
-            duration: "30",
-            questions: "45",
-        }, {
-            id: "2",
-            title: "Python",
-            duration: "24",
-            questions: "30",
-        },
-    ];
+export default async function CardContainer() {
+    async function readData() {
+        "use server";
 
-    const listItems = quizList.map(quiz => <Card title={quiz.title} key={quiz.id} duration={quiz.duration + " Minutes"} questions={quiz.questions + " Questions"}/>)
+        const sql = neon(process.env.DATABASE_URL);
+        const data = await sql`SELECT title, description, duration FROM tryouts`
 
-    return (
-        <div className="flex flex-col text-[#fafafa] mt-32 gap-16 font-[Roboto_Mono] w-screen text-center">
-            <span className="text-4xl">Available Quizzes</span>
-            <div className="flex flex-wrap gap-4 w-screen items-center justify-center">
-                {listItems}
+        const items = data.map(quiz => <Card title={quiz.title} duration={quiz.duration + " Minutes"} questions={0 + " Questions"}/>)
+
+        return (
+            <div className="flex flex-col text-[#fafafa] mt-32 gap-16 font-[Roboto_Mono] w-screen text-center">
+                <span className="text-4xl">Available Quizzes</span>
+                <div className="flex flex-wrap gap-4 w-screen items-center justify-center">
+                    {items}
+                </div>
             </div>
-        </div>
-    );
+        );
+    }
+
+    return readData()
 }
